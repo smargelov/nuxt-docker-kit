69bcb8e89e99e413413a8afab3bb8dd32ced538e
Sergey Margelov <smargelov@gmail.com>
2022-03-19 23:23:50 +0300

    Add sass setting

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,47 +1,57 @@
 export default {
-	// Global page headers: https://go.nuxtjs.dev/config-head
-	head: {
-		title: 'nuxt-docker-kit',
-		htmlAttrs: {
-			lang: 'en'
-		},
-		meta: [
-			{ charset: 'utf-8' },
-			{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
-			{ hid: 'description', name: 'description', content: '' },
-			{ name: 'format-detection', content: 'telephone=no' }
-		],
-		link: [
-			{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-		]
-	},
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'Start nuxt kit with docker, nginx, jest, linters, sass, pug etc',
+    htmlAttrs: {
+      lang: 'en'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
 
-	// Global CSS: https://go.nuxtjs.dev/config-css
-	css: [],
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+    '~/assets/sass/main.sass'
+  ],
 
-	// Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-	plugins: [],
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [],
 
-	// Auto import components: https://go.nuxtjs.dev/config-components
-	components: true,
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
 
-	// Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-	buildModules: [
-		// https://go.nuxtjs.dev/eslint
-		'@nuxtjs/eslint-module',
-		// https://go.nuxtjs.dev/stylelint
-		'@nuxtjs/stylelint-module'
-	],
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    // https://go.nuxtjs.dev/eslint
+    '@nuxtjs/eslint-module'
+  ],
 
-	// Modules: https://go.nuxtjs.dev/config-modules
-	modules: [
-		// https://go.nuxtjs.dev/axios
-		'@nuxtjs/axios'
-	],
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    // https://go.nuxtjs.dev/axios
+    '@nuxtjs/axios'
+  ],
 
-	// Axios module configuration: https://go.nuxtjs.dev/config-axios
-	axios: {},
+  // Axios module configuration: https://go.nuxtjs.dev/config-axios
+  axios: {},
 
-	// Build Configuration: https://go.nuxtjs.dev/config-build
-	build: {}
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+    loaders: {
+      sass: {
+        additionalData: `
+          @use "sass:math"
+          @import "@/assets/sass/utils/vars.sass"
+          @import "@/assets/sass/utils/mixins.sass"
+        `
+      }
+    }
+  }
 }
